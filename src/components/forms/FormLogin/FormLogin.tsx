0a98195f7c074571e94aa5b6c16383faff3763d0
NinZZya <s.a.azarov@gmail.com>
2020-08-12 22:57:04 +0300

    Create FormLogin

--- src/components/forms/FormLogin/FormLogin.tsx
+++ src/components/forms/FormLogin/FormLogin.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Action } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { connect } from 'react-redux';
+import { Form, Input, Button, message } from 'antd';
+import { UserOutlined, LockOutlined, LoginOutlined } from '@ant-design/icons';
+import { Operation } from '../../../reducer/user/user';
+import { getAuthorizationStatus } from '../../../reducer/user/selectors';
+import { AuthorizationStatus } from '../../../const';
+import { TAppState, TAuthData } from '../../../types';
+interface TProps {
+  authorizationStatus: string;
+  login: (authData: TAuthData) => void;
+}
 
+const FormLogin = (props: TProps) => {
+  const { authorizationStatus, login } = props;
 
-const FormLogin = () => {
+  if (authorizationStatus === AuthorizationStatus.AUTH_ERROR) {
+    message.error('Bad login or password');
+  }
+
   return (
     <Form
       name="normal_login"
       className="login-form"
       initialValues={{ remember: true }}
-      onFinish={() => console.log('Finish')}
+      onFinish={(authData) => login({
+        login: authData.login,
+        password: authData.password,
+      })}
       style={{ padding: '50px', border: '1px solid #d9d9d9' }}
     >
       <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}
+        name="login"
+        rules={[{ required: true, message: 'Please input your Login!' }]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Login" />
       </Form.Item>
       <Form.Item
         name="password"
@@ -29,12 +48,24 @@ const FormLogin = () => {
         />
       </Form.Item>
       <Form.Item style={{ textAlign: 'center' }}>
-        <Button type="primary" htmlType="submit" className="login-form-button" style={{ width: '150px' }}>
-          Log in
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          <LoginOutlined /> Log in
         </Button>
       </Form.Item>
     </Form>
   );
 };
 
-export default FormLogin;
+export { FormLogin };
+
+const mapStateToProps = (state: TAppState) => ({
+  authorizationStatus: getAuthorizationStatus(state),
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<TAppState, void, Action>) => ({
+  login(authData: TAuthData) {
+    dispatch(Operation.login(authData))
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FormLogin);
