6102485246b2b8c0e6a51362080d1ba4d99f7ba8
NinZZya <s.a.azarov@gmail.com>
2020-08-12 23:41:00 +0300

    Review rootReducer. Code review

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -1,6 +1,6 @@
 import NameSpace from '../name-space';
 
-const NAME_SPACE = NameSpace.USERS;
+const NAME_SPACE = NameSpace.USER;
 
 export const getAuthorizationStatus = (state) => {
   return state[NAME_SPACE].authorizationStatus;
