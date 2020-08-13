700366ddfd4d2d4025b11ec1bbe6eda25250573e
NinZZya <s.a.azarov@gmail.com>
2020-08-13 10:26:21 +0300

    Change routing

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -6,13 +6,18 @@ import {
 } from 'react-router-dom';
 import HeaderApp from '../HeaderApp';
 import Main from '../../pages/Main';
+import Board from '../../pages/Board';
+import { AppRout } from '../../const';
 
 const Routes = () => {
   return (
     <Router>
       <HeaderApp />
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path={AppRout.ROOT} component={Main} />
+      </Switch>
+      <Switch>
+        <Route exact path={`${AppRout.BOARD}/:boardId`} component={Board} />
       </Switch>
     </Router>
   );
