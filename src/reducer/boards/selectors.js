c1acad0a5c9f655d5d64c73e9c00c47c466e706f
NinZZya <s.a.azarov@gmail.com>
2020-08-13 16:58:01 +0300

    Code review

--- src/reducer/boards/selectors.js
+++ src/reducer/boards/selectors.js
@@ -1,16 +1,16 @@
 import NameSpace from '../name-space';
 
-const BOARDS = NameSpace.BOARDS;
+const BOARDS_NAME = NameSpace.BOARDS;
 
 export const getActiveBoardId = (state) => {
-  return state[BOARDS].activeBoardId;
+  return state[BOARDS_NAME].activeBoardId;
 };
 
-export const getBoard = (state, boardId = state[BOARDS].activeBoardId) => {
-  return state[BOARDS].boards[boardId];
+export const getBoard = (state, boardId = state[BOARDS_NAME].activeBoardId) => {
+  return state[BOARDS_NAME].boards[boardId];
 };
 
 export const getBoards = (state) => {
-  const boards = state[BOARDS].boards;
+  const boards = state[BOARDS_NAME].boards;
   return boards ? Object.values(boards) : [];
 };
