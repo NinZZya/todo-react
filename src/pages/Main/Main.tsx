4b5aa7aa3eb8b6772959f5bfdda145fbcfe72990
NinZZya <s.a.azarov@gmail.com>
2020-08-13 21:47:08 +0300

    Code review Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 import { Layout, Spin } from 'antd';
 import ListBoards from '../../components/lists/ListBoards';
 import { getBoardsStatus } from '../../reducer/boards/selectors';
-import { ActionCreator } from '../../reducer/boards/boards';
-import { TAppState, TStatus, TBoards, TId } from '../../types';
+import { TAppState, TStatus } from '../../types';
 import { Status } from '../../const';
 
-const { Content } = Layout;
-
 interface TProps {
-  boards: TBoards | null;
   boardsStatus: TStatus;
 };
+
+const { Content } = Layout;
 
 const Main = (props: TProps) => {
   const { boardsStatus } = props;
@@ -35,10 +32,4 @@ const mapStateToPorps = (state: TAppState) => ({
   boardsStatus: getBoardsStatus(state),
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  setActiveBoardId(activeBoardId: TId) {
-    dispatch(ActionCreator.setActiveBoardId(activeBoardId))
-  },
-});
-
-export default connect(mapStateToPorps, mapDispatchToProps)(Main);
+export default connect(mapStateToPorps)(Main);
