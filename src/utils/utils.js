cae4c8b2c65801feab7a9263378ed00bf2e2c96a
NinZZya <s.a.azarov@gmail.com>
2020-08-13 21:46:16 +0300

    Add getPathKeyValue

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,10 @@
 export const extend = (a, b) => {
   return Object.assign({}, a, b);
 };
+
+export const getPathKeyValue = (path, key) => {
+  const pathValues = path.split('/');
+  const keyIndex = pathValues.indexOf(key);
+
+  return keyIndex !== -1 ? pathValues[keyIndex + 1] : null;
+};
