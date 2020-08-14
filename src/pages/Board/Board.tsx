d473d6955eb71c0a2920a171bd03e427cc2b4d3b
NinZZya <s.a.azarov@gmail.com>
2020-08-14 13:28:41 +0300

    Add ListTasks. Review Boar page

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Layout, Spin } from 'antd';
+import ListTasks from './components/ListTasks';
+import * as tasksSelectors from '../../reducer/tasks/selectors';
+import { LoadingStatus } from '../../const';
+import { IAppState } from '../../types';
 
+interface BoardProps {
+  tasksStatus: LoadingStatus;
+};
 
 const { Content } = Layout;
 
-const Board: React.FC = () => {
+const Board: React.FC<BoardProps> = (props) => {
+  const { tasksStatus } = props;
+
+  if (tasksStatus !== LoadingStatus.SUCCESS) {
+    return <Spin style={{ padding: '50px', width: '100%' }} tip="Loading..."></Spin>
+  }
 
   return (
     <Content style={{ padding: '50px' }}>
-      Board page
-      <Spin style={{ padding: '50px', width: '100%' }} tip="Loading..."></Spin>
+      <ListTasks />
     </Content>
   );
 };
 
-export default Board;
+export { Board };
+
+const mapStateToPorps = (state: IAppState) => ({
+  tasksStatus: tasksSelectors.getTasksStatus(state),
+});
+
+export default connect(mapStateToPorps)(Board);
