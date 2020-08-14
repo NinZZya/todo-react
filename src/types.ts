900a951685fb7744642602f0a14ad52b81229f21
NinZZya <s.a.azarov@gmail.com>
2020-08-14 04:24:16 +0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,26 @@
 import store from './store';
+import { UserStatus } from './const';
+import { NameSpace } from './reducer/name-space';
 
 export type TAppState = typeof store;
+
+export interface IUserState {
+  userStatus: UserStatus.NO_AUTH;
+  user: any;
+};
+
+export interface IAppState {
+  [NameSpace.USER]: IUserState;
+};
 
 export type TAuthData = {
   login: string,
   password: string,
+};
+
+export type TId = number;
+
+export interface IUser {
+  id: TId;
+  login: string;
 };
