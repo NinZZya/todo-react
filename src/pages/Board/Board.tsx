71a5b77f1af5cf391ae920fe166c4b770073a25d
NinZZya <s.a.azarov@gmail.com>
2020-08-14 15:19:56 +0300

    Add routing papams

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import { Action } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
+import { useParams } from 'react-router-dom';
 import { Layout, Spin } from 'antd';
 import ListTasks from './components/ListTasks';
 import FilterTasks from './components/FilterTasks';
@@ -10,21 +11,39 @@ import * as boardsSelector from '../../reducer/boards/selectors';
 import * as tasksSelector from '../../reducer/tasks/selectors';
 import { boardsOperation } from '../../reducer/boards/boards';
 import { tasksOperation } from '../../reducer/tasks/tasks';
+import { boardsActionCreator } from '../../reducer/boards/boards';
 import { LoadingStatus } from '../../const';
 import { IAppState, TId, IUser } from '../../types';
 
 interface BoardProps {
   boardsStatus: LoadingStatus;
   tasksStatus: LoadingStatus;
+  activeBoardId: TId;
   user: IUser | null;
   loadBoards: (userId: TId) => void;
   loadTasks: (userId: TId) => void;
+  setActiveBoardId: (boardId: TId) => void;
 };
 
 const { Content } = Layout;
 
 const Board: React.FC<BoardProps> = (props) => {
-  const { boardsStatus, tasksStatus, user, loadBoards, loadTasks } = props;
+  const {
+    boardsStatus,
+    tasksStatus,
+    activeBoardId,
+    user,
+    loadBoards,
+    loadTasks,
+    setActiveBoardId,
+  } = props;
+
+  const params: any = useParams();
+  const boardId = !isNaN(Number(params.boardId)) ? Number(params.boardId) : -1;
+
+  if (boardId !== -1 && activeBoardId !== boardId) {
+    setActiveBoardId(boardId);
+  }
 
   useEffect(() => {
     if (user && boardsStatus === LoadingStatus.INIT) {
@@ -55,6 +74,7 @@ export { Board };
 const mapStateToPorps = (state: IAppState) => ({
   boardsStatus: boardsSelector.getBoardsStatus(state),
   tasksStatus: tasksSelector.getTasksStatus(state),
+  activeBoardId: boardsSelector.getActiveBoardId(state),
   user: userSelector.getUser(state),
 });
 
@@ -65,6 +85,9 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<IAppState, void, Action>) =>
     },
     loadTasks(userId: TId) {
       dispatch(tasksOperation.loadTasks(userId));
+    },
+    setActiveBoardId(boardId: TId) {
+      dispatch(boardsActionCreator.setActiveBoardId(boardId))
     },
   }
 };
