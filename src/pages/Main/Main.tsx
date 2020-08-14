a6a3146281381dd743c2d4c900ae69cd7d7cc206
NinZZya <s.a.azarov@gmail.com>
2020-08-14 13:47:17 +0300

    Code review

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -24,6 +24,7 @@ const { Content } = Layout;
 
 const Main: React.FC<MainProps> = (props) => {
   const { boardsStatus, tasksStatus, user, loadBoards, loadTasks } = props;
+
   useEffect(() => {
     if (user && boardsStatus === LoadingStatus.INIT) {
       loadBoards(user.id);
