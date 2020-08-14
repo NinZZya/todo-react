3100250c07e537040b25ded01f97a07a7c079d4d
NinZZya <s.a.azarov@gmail.com>
2020-08-14 12:46:49 +0300

    Add task reducer to root reducer

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import { NameSpace } from './name-space';
 import { userReducer } from './user/user';
 import { boardsReducer } from './boards/boards';
+import { tasksReducer } from './tasks/tasks';
 
 const rootReducer = combineReducers({
   [NameSpace.USER]: userReducer,
   [NameSpace.BOARDS]: boardsReducer,
+  [NameSpace.TASKS]: tasksReducer,
 });
 
 export default rootReducer;
