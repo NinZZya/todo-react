0e33c8ba60c1fefe85fab58cd3708fc6a9989eee
NinZZya <s.a.azarov@gmail.com>
2020-08-14 06:46:43 +0300

    WIP. Add boards reducer and selectors

---
+++ src/reducer/boards/selectorst.ts
@@ -0,0 +1,17 @@
+import { NameSpace } from '../name-space';
+import { IAppState } from '../../types';
+
+const BOARDS_NAME = NameSpace.BOARDS;
+
+export const getActiveBoardId = (state: IAppState) => state[BOARDS_NAME].activeBoardId;
+export const getBoardsStatus = (state: IAppState) => state[BOARDS_NAME].boardsStatus;
+
+export const getBoard = (state: IAppState, boardId = state[BOARDS_NAME].activeBoardId) => {
+  const boards = state[BOARDS_NAME].boards;
+  return boards !== null ? boards[boardId] : null;
+};
+
+export const getBoards = (state: IAppState) => {
+  const boards = state[BOARDS_NAME].boards;
+  return boards ? Object.values(boards) : [];
+};
