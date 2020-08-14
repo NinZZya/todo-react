ffca4edfa9bbb72ef27c255aa39db751a866c68c
NinZZya <s.a.azarov@gmail.com>
2020-08-14 13:46:54 +0300

    Code review

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -6,14 +6,16 @@ import { Layout, Spin } from 'antd';
 import ListBoards from './components/ListBoards';
 import { boardsOperation } from '../../reducer/boards/boards';
 import { tasksOperation } from '../../reducer/tasks/tasks';
-import * as boardsSelector from '../../reducer/boards/selectors'
-import * as  userSelector from '../../reducer/user/selectors';
+import * as boardsSelector from '../../reducer/boards/selectors';
+import * as taskssSelector from '../../reducer/tasks/selectors'
+import * as userSelector from '../../reducer/user/selectors';
 import { LoadingStatus } from '../../const';
 import { IAppState, IUser, TId } from '../../types';
 
 interface MainProps {
   user: IUser | null,
   boardsStatus: LoadingStatus;
+  tasksStatus: LoadingStatus;
   loadBoards: (userId: TId) => void;
   loadTasks: (userId: TId) => void;
 };
@@ -21,10 +23,13 @@ interface MainProps {
 const { Content } = Layout;
 
 const Main: React.FC<MainProps> = (props) => {
-  const { boardsStatus, user, loadBoards, loadTasks } = props;
+  const { boardsStatus, tasksStatus, user, loadBoards, loadTasks } = props;
   useEffect(() => {
     if (user && boardsStatus === LoadingStatus.INIT) {
       loadBoards(user.id);
+    }
+
+    if (user && tasksStatus === LoadingStatus.INIT) {
       loadTasks(user.id);
     }
   });
@@ -44,6 +49,7 @@ export { Main };
 
 const mapStateToPorps = (state: IAppState) => ({
   boardsStatus: boardsSelector.getBoardsStatus(state),
+  tasksStatus: taskssSelector.getTasksStatus(state),
   user: userSelector.getUser(state),
 });
 
